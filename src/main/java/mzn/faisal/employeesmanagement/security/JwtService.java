7e6eb449c9ab23442f26e133b5c1a28176d9fb5c
faisal albarq <faisal@mzn.dev>
2026-07-29 09:51:40 +0300

    Add JWT service for token handling and configure JWT properties

---
+++ src/main/java/mzn/faisal/employeesmanagement/security/JwtService.java
@@ -0,0 +1,36 @@
+package mzn.faisal.employeesmanagement.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+
+@Service
+public class JwtService {
+
+    @Value("${jwt.key}")
+    private String secret;
+
+    @Value("${jwt.access-token-expiration}")
+    private Long expiration;
+
+    private SecretKey getSecretKey() {
+        return Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
+
+    public Claims getAllClaimsFromToken(String token){
+        return Jwts.parser()
+                .verifyWith(getSecretKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+
+
+}
