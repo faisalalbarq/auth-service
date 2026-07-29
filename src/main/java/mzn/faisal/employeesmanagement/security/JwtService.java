23ef95b2401690cc5fe19b6dc717eff984a7d247
faisal albarq <faisal@mzn.dev>
2026-07-29 10:59:51 +0300

    Add method to generate JWT tokens in JwtService

--- src/main/java/mzn/faisal/employeesmanagement/security/JwtService.java
+++ src/main/java/mzn/faisal/employeesmanagement/security/JwtService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
+import java.util.Date;
 
 @Service
 public class JwtService {
@@ -31,6 +32,16 @@ public class JwtService {
                 .getPayload();
     }
 
+    public String generateToken(String username){
+        Date now = new Date();
+        Date expirationDate = new Date(now.getTime() + expiration);
 
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(now)
+                .expiration(expirationDate)
+                .signWith(getSecretKey())
+                .compact();
+    }
 
 }
