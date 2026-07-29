25c6cffc5138edeaec48f2e30e3de0614152292e
faisal albarq <faisal@mzn.dev>
2026-07-29 15:24:52 +0300

    Add methods for extracting claims and checking token expiry in JwtService

--- src/main/java/mzn/faisal/employeesmanagement/security/JwtService.java
+++ src/main/java/mzn/faisal/employeesmanagement/security/JwtService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
@@ -24,6 +25,19 @@ public class JwtService {
     }
 
 
+    public String getUsernameFromToken(String token) {
+        return getClaimFromToken(token, Claims::getSubject);
+    }
+
+    public Date getExpirationDateFromToken(String token) {
+        return getClaimFromToken(token, Claims::getExpiration);
+    }
+
+    public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = getAllClaimsFromToken(token);
+        return claimsResolver.apply(claims);
+    }
+
     public Claims getAllClaimsFromToken(String token){
         return Jwts.parser()
                 .verifyWith(getSecretKey())
@@ -32,10 +46,16 @@ public class JwtService {
                 .getPayload();
     }
 
+    public Boolean isTokenExpired(String token){
+        final Date expiration = getExpirationDateFromToken(token);
+        return expiration.before(new Date());
+    }
+
     public String generateToken(String username){
         Date now = new Date();
         Date expirationDate = new Date(now.getTime() + expiration);
 
+        // getTime(): return the time in millisecond
         return Jwts.builder()
                 .subject(username)
                 .issuedAt(now)
