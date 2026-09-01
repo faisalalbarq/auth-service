72e287b6f885023bd1040cc3a7dec785e4e81f10
faisal albarq <faisal@mzn.dev>
2026-09-01 08:41:35 +0300

    Add CORS configuration and SystemProperty class for managing environment-specific properties

--- src/main/java/mzn/faisal/employeesmanagement/config/SecurityConfig.java
+++ src/main/java/mzn/faisal/employeesmanagement/config/SecurityConfig.java
@@ -4,9 +4,33 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
+
+    private final SystemProperty systemProperty;
+    public SecurityConfig(SystemProperty systemProperty) {
+        this.systemProperty = systemProperty;
+    }
+
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedOrigins(List.of(systemProperty.getFrontendDomain()));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
