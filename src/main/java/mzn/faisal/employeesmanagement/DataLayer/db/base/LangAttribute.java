33e1502ac73bb84f2b389186d2c18a7b48925334
faisal albarq <faisal@mzn.dev>
2026-09-15 11:35:13 +0300

    Add LangAttribute and LangAttributeConverter for multilingual support

---
+++ src/main/java/mzn/faisal/employeesmanagement/DataLayer/db/base/LangAttribute.java
@@ -0,0 +1,16 @@
+package mzn.faisal.employeesmanagement.DataLayer.db.base;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class LangAttribute {
+    private String ar;
+    private String en;
+
+}
