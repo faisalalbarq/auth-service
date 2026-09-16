0a98ca7456a5b4af66f44a4a7f0edf4dd443ccb3
faisal albarq <faisal@mzn.dev>
2026-09-16 13:05:07 +0300

    Remove EmployeeRepository interface

--- src/main/java/mzn/faisal/employeesmanagement/DataLayer/Repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-//package mzn.faisal.employeesmanagement.DataLayer.Repository;
-//
-//import mzn.faisal.employeesmanagement.DataLayer.Entity.Employee;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import java.util.UUID;
-//
-//public interface EmployeeRepository extends JpaRepository<Employee, UUID> {
-//}
